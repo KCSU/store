ed42295bff73e7be9de7456528122b7fc1712b1e
Geno Racklin Asher <geno.racklinasher@gmail.com>
2022-01-17 19:24:47 +0000

    Add tests to ticket handlers

--- backend/handlers/tickets_test.go
+++ backend/handlers/tickets_test.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"strconv"
+	"strings"
 	"testing"
 	"time"
 
@@ -22,11 +23,12 @@ import (
 
 type TicketSuite struct {
 	suite.Suite
-	h       *Handler
-	auth    *am.Auth
-	users   *sm.UserStore
-	tickets *sm.TicketStore
-	formals *sm.FormalStore
+	h        *Handler
+	auth     *am.Auth
+	users    *sm.UserStore
+	tickets  *sm.TicketStore
+	formals  *sm.FormalStore
+	mockUser *model.User
 }
 
 func (t *TicketSuite) AssertExpectations() {
@@ -52,14 +54,15 @@ func (t *TicketSuite) SetupTest() {
 	// Config?
 	// Some mocks we always want to use
 	t.auth.On("GetUserId", mock.Anything).Maybe().Return(userId)
-	t.users.On("Find", userId).Maybe().Return(model.User{
+	t.mockUser = &model.User{
 		Model: model.Model{
 			ID: uint(userId),
 		},
 		Name:           "Logan Roy",
 		Email:          "lry555@cam.ac.uk",
 		ProviderUserId: "abcdefg",
-	}, nil)
+	}
+	t.users.On("Find", userId).Maybe().Return(*t.mockUser, nil)
 }
 
 func (t *TicketSuite) TestGetTickets() {
@@ -194,6 +197,146 @@ func (t *TicketSuite) TestGetTickets() {
 	t.AssertExpectations()
 }
 
+func (t *TicketSuite) TestBuyTicket() {
+	type wants struct {
+		code    int
+		message string
+	}
+	type test struct {
+		name   string
+		body   string
+		formal model.Formal
+		exists bool
+		wants  *wants
+	}
+	userGroups := []model.Group{
+		{Model: model.Model{ID: 1}},
+		{Model: model.Model{ID: 3}},
+	}
+	tests := []test{
+		{
+			"Insufficient Groups",
+			`{
+				"formalId": 1,
+				"ticket": {"option": "Normal"},
+				"guestTickets": []	
+			}`,
+			model.Formal{
+				Model: model.Model{ID: 1},
+				Name:  "Wrong Group Formal",
+				Groups: []model.Group{
+					{Model: model.Model{ID: 2}},
+					{Model: model.Model{ID: 4}},
+				},
+			},
+			false,
+			&wants{http.StatusForbidden, "Forbidden"},
+		},
+		{
+			"Guest Limit",
+			`{
+				"formalId": 2,
+				"ticket": {"option": "Normal"},
+				"guestTickets": [
+					{"option": "Pescetarian"},
+					{"option": "Vegan"}
+				]	
+			}`,
+			model.Formal{
+				Model:      model.Model{ID: 2},
+				Name:       "Wrong Number Formal",
+				GuestLimit: 1,
+				Groups: []model.Group{
+					{Model: model.Model{ID: 3}},
+					{Model: model.Model{ID: 4}},
+				},
+			},
+			false,
+			&wants{http.StatusUnprocessableEntity, "Too many guest tickets requested."},
+		},
+		{
+			"Duplicate Ticket",
+			`{
+				"formalId": 3,
+				"ticket": {"option": "Normal"},
+				"guestTickets": [
+					{"option": "Pescetarian"},
+					{"option": "Vegan"}
+				]	
+			}`,
+			model.Formal{
+				Model:      model.Model{ID: 3},
+				Name:       "Existing Formal",
+				GuestLimit: 2,
+				Groups: []model.Group{
+					{Model: model.Model{ID: 3}},
+					{Model: model.Model{ID: 1}},
+				},
+			},
+			true,
+			&wants{http.StatusConflict, "Ticket already exists."},
+		},
+		{
+			"Should Create",
+			`{
+				"formalId": 4,
+				"ticket": {"option": "Normal"},
+				"guestTickets": [
+					{"option": "Pescetarian"},
+					{"option": "Vegan"}
+				]	
+			}`,
+			model.Formal{
+				Model:      model.Model{ID: 4},
+				Name:       "Wrong Group Formal",
+				GuestLimit: 3,
+				Groups: []model.Group{
+					{Model: model.Model{ID: 1}},
+					{Model: model.Model{ID: 2}},
+				},
+			},
+			false,
+			nil,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func() {
+			// HTTP
+			e := echo.New()
+			req := httptest.NewRequest(
+				http.MethodPost, "/tickets", strings.NewReader(test.body),
+			)
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+			// Mock
+			t.formals.On("Find", int(test.formal.ID)).Return(test.formal, nil).Once()
+			t.users.On("Groups", t.mockUser).Return(userGroups, nil)
+			t.tickets.On(
+				"ExistsByFormal", int(test.formal.ID), userId,
+			).Maybe().Return(test.exists, nil)
+			if test.wants == nil {
+				t.tickets.On(
+					"BatchCreate", mock.AnythingOfType("[]model.Ticket"),
+				).Return(nil).Once()
+			}
+			// Test
+			err := t.h.BuyTicket(c)
+			if test.wants != nil {
+				var he *echo.HTTPError
+				if t.ErrorAs(err, &he) {
+					t.Equal(test.wants.code, he.Code)
+					t.Equal(test.wants.message, he.Message)
+				}
+			} else {
+				t.NoError(err)
+				t.Equal(http.StatusCreated, rec.Code)
+			}
+		})
+	}
+	t.AssertExpectations()
+}
+
 func (t *TicketSuite) TestCancelTickets() {
 	// HTTP
 	e := echo.New()
@@ -217,23 +360,24 @@ func (t *TicketSuite) TestCancelTicket() {
 		message string
 	}
 	type test struct {
+		name   string
 		ticket model.Ticket
 		wants  *wants
 	}
 	tests := []test{
-		{model.Ticket{
+		{"Incorrect User", model.Ticket{
 			UserId:   userId + 1,
 			FormalID: 53,
 			IsGuest:  true,
 			IsQueue:  false,
 		}, &wants{http.StatusForbidden, "Forbidden"}},
-		{model.Ticket{
+		{"Forbid Non-Guest", model.Ticket{
 			UserId:   userId,
 			FormalID: 54,
 			IsGuest:  false,
 			IsQueue:  true,
 		}, &wants{http.StatusForbidden, "Non-guest tickets must be cancelled as a group"}},
-		{model.Ticket{
+		{"Should Cancel", model.Ticket{
 			UserId:   userId,
 			FormalID: 55,
 			IsGuest:  true,
@@ -241,31 +385,33 @@ func (t *TicketSuite) TestCancelTicket() {
 		}, nil},
 	}
 	for _, test := range tests {
-		// HTTP
-		e := echo.New()
-		req := new(http.Request)
-		rec := httptest.NewRecorder()
-		c := e.NewContext(req, rec)
-		c.SetParamNames("id")
-		ticketId := 84
-		c.SetParamValues(strconv.Itoa(ticketId))
-		// Mock
-		t.tickets.On("Find", ticketId).Return(test.ticket, nil).Once()
-		if test.wants == nil {
-			t.tickets.On("Delete", ticketId).Return(nil).Once()
-		}
-		// Test
-		err := t.h.CancelTicket(c)
-		if test.wants != nil {
-			var he *echo.HTTPError
-			if t.ErrorAs(err, &he) {
-				t.Equal(test.wants.code, he.Code)
-				t.Equal(test.wants.message, he.Message)
+		t.Run(test.name, func() {
+			// HTTP
+			e := echo.New()
+			req := new(http.Request)
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+			c.SetParamNames("id")
+			ticketId := 84
+			c.SetParamValues(strconv.Itoa(ticketId))
+			// Mock
+			t.tickets.On("Find", ticketId).Return(test.ticket, nil).Once()
+			if test.wants == nil {
+				t.tickets.On("Delete", ticketId).Return(nil).Once()
 			}
-		} else {
-			t.NoError(err)
-			t.Equal(http.StatusOK, rec.Code)
-		}
+			// Test
+			err := t.h.CancelTicket(c)
+			if test.wants != nil {
+				var he *echo.HTTPError
+				if t.ErrorAs(err, &he) {
+					t.Equal(test.wants.code, he.Code)
+					t.Equal(test.wants.message, he.Message)
+				}
+			} else {
+				t.NoError(err)
+				t.Equal(http.StatusOK, rec.Code)
+			}
+		})
 	}
 	t.AssertExpectations()
 }
@@ -276,18 +422,19 @@ func (t *TicketSuite) TestEditTicket() {
 		message string
 	}
 	type test struct {
+		name   string
 		ticket model.Ticket
 		option string
 		wants  *wants
 	}
 	tests := []test{
-		{model.Ticket{
+		{"Incorrect User", model.Ticket{
 			UserId:   userId + 1,
 			FormalID: 53,
 			IsGuest:  true,
 			IsQueue:  false,
 		}, "Vegetarian", &wants{http.StatusForbidden, "Forbidden"}},
-		{model.Ticket{
+		{"Should Update", model.Ticket{
 			UserId:   userId,
 			FormalID: 55,
 			IsGuest:  true,
@@ -296,42 +443,44 @@ func (t *TicketSuite) TestEditTicket() {
 	}
 	// HTTP
 	for _, test := range tests {
-		ticketId := 95
-		route := fmt.Sprintf("/tickets/%d", ticketId)
-		body, err := json.Marshal(map[string]string{
-			"option": test.option,
-		})
-		t.Require().NoError(err)
-		e := echo.New()
-		req := httptest.NewRequest(
-			http.MethodPut, route, bytes.NewReader(body),
-		)
-		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-		rec := httptest.NewRecorder()
-		c := e.NewContext(req, rec)
-		c.SetParamNames("id")
-		c.SetParamValues(strconv.Itoa(ticketId))
-		// Mock
-		test.ticket.ID = uint(ticketId)
-		t.tickets.On("Find", ticketId).Return(test.ticket, nil).Once()
-		if test.wants == nil {
-			t.tickets.On(
-				"Update", ticketId,
-				&dto.TicketRequestDto{MealOption: test.option},
-			).Return(nil).Once()
-		}
-		// Test
-		err = t.h.EditTicket(c)
-		if test.wants != nil {
-			var he *echo.HTTPError
-			if t.ErrorAs(err, &he) {
-				t.Equal(test.wants.code, he.Code)
-				t.Equal(test.wants.message, he.Message)
+		t.Run(test.name, func() {
+			ticketId := 95
+			route := fmt.Sprintf("/tickets/%d", ticketId)
+			body, err := json.Marshal(map[string]string{
+				"option": test.option,
+			})
+			t.Require().NoError(err)
+			e := echo.New()
+			req := httptest.NewRequest(
+				http.MethodPut, route, bytes.NewReader(body),
+			)
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+			c.SetParamNames("id")
+			c.SetParamValues(strconv.Itoa(ticketId))
+			// Mock
+			test.ticket.ID = uint(ticketId)
+			t.tickets.On("Find", ticketId).Return(test.ticket, nil).Once()
+			if test.wants == nil {
+				t.tickets.On(
+					"Update", ticketId,
+					&dto.TicketRequestDto{MealOption: test.option},
+				).Return(nil).Once()
 			}
-		} else {
-			t.NoError(err)
-			t.Equal(http.StatusOK, rec.Code)
-		}
+			// Test
+			err = t.h.EditTicket(c)
+			if test.wants != nil {
+				var he *echo.HTTPError
+				if t.ErrorAs(err, &he) {
+					t.Equal(test.wants.code, he.Code)
+					t.Equal(test.wants.message, he.Message)
+				}
+			} else {
+				t.NoError(err)
+				t.Equal(http.StatusOK, rec.Code)
+			}
+		})
 	}
 	t.AssertExpectations()
 }
@@ -342,17 +491,20 @@ func (t *TicketSuite) TestAddTicket() {
 		message string
 	}
 	type test struct {
+		name   string
 		formal model.Formal
 		option string
 		wants  *wants
 	}
 	tests := []test{{
+		"Should Add",
 		model.Formal{
 			GuestLimit: 4,
 		},
 		"Vegetarian",
 		nil,
 	}, {
+		"Guest Limit",
 		model.Formal{
 			GuestLimit: 2,
 		},
@@ -360,47 +512,50 @@ func (t *TicketSuite) TestAddTicket() {
 		&wants{http.StatusUnprocessableEntity, "Too many guest tickets requested."},
 	}}
 	for _, test := range tests {
-		// HTTP
-		formalId := 56
-		path := fmt.Sprintf("/formals/%d", formalId)
-		e := echo.New()
-		body, err := json.Marshal(map[string]string{
-			"option": test.option,
-		})
-		t.Require().NoError(err)
-		req := httptest.NewRequest(http.MethodPost, path, bytes.NewReader(body))
-		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-		rec := httptest.NewRecorder()
-		c := e.NewContext(req, rec)
-		c.SetParamNames("id")
-		c.SetParamValues(strconv.Itoa(formalId))
-		// Mock
-		t.tickets.On("ExistsByFormal", formalId, userId).Return(true, nil).Once()
-		// The user already has 2 guest tickets
-		t.tickets.On("CountGuestByFormal", formalId, userId).Return(int64(2), nil).Once()
-		t.formals.On("Find", formalId).Return(test.formal, nil).Once()
-		if test.wants == nil {
-			t.tickets.On("Create", &model.Ticket{
-				FormalID:   formalId,
-				IsGuest:    true,
-				IsQueue:    true,
-				MealOption: test.option,
-				UserId:     userId,
-			}).Return(nil).Once()
-		}
-		// Test
-		err = t.h.AddTicket(c)
-		if test.wants != nil {
-			var he *echo.HTTPError
-			if t.ErrorAs(err, &he) {
-				t.Equal(test.wants.code, he.Code)
-				t.Equal(test.wants.message, he.Message)
+		t.Run(test.name, func() {
+			// HTTP
+			formalId := 56
+			path := fmt.Sprintf("/formals/%d", formalId)
+			e := echo.New()
+			body, err := json.Marshal(map[string]string{
+				"option": test.option,
+			})
+			t.Require().NoError(err)
+			req := httptest.NewRequest(http.MethodPost, path, bytes.NewReader(body))
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+			c.SetParamNames("id")
+			c.SetParamValues(strconv.Itoa(formalId))
+			// Mock
+			t.tickets.On("ExistsByFormal", formalId, userId).Return(true, nil).Once()
+			// The user already has 2 guest tickets
+			t.tickets.On("CountGuestByFormal", formalId, userId).Return(int64(2), nil).Once()
+			t.formals.On("Find", formalId).Return(test.formal, nil).Once()
+			if test.wants == nil {
+				t.tickets.On("Create", &model.Ticket{
+					FormalID:   formalId,
+					IsGuest:    true,
+					IsQueue:    true,
+					MealOption: test.option,
+					UserId:     userId,
+				}).Return(nil).Once()
 			}
-		} else {
-			t.NoError(err)
-			t.Equal(http.StatusCreated, rec.Code)
-		}
+			// Test
+			err = t.h.AddTicket(c)
+			if test.wants != nil {
+				var he *echo.HTTPError
+				if t.ErrorAs(err, &he) {
+					t.Equal(test.wants.code, he.Code)
+					t.Equal(test.wants.message, he.Message)
+				}
+			} else {
+				t.NoError(err)
+				t.Equal(http.StatusCreated, rec.Code)
+			}
+		})
 	}
+	t.AssertExpectations()
 }
 
 func TestTicketSuite(t *testing.T) {
