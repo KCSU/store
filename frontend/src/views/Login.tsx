5de8708ca97f2ac7dab8473c403aff66487359ad
Geno Racklin Asher <geno.racklinasher@gmail.com>
2022-02-05 12:22:14 +0000

    Firebase hosting

--- frontend/src/views/Login.tsx
+++ frontend/src/views/Login.tsx
@@ -12,7 +12,7 @@ import ravenImg from "../img/raven.png";
 export function Login() {
   const location = useLocation();
   const { data: user, isError, isLoading } = useAuthUser();
-  const from: string = location.state?.from ?? "/";
+  const from: string = (location.state as any)?.from ?? "/";
   const filter = useColorModeValue("invert()", undefined);
   const redirectUrl = import.meta.env.VITE_API_BASE_URL + 'auth/redirect';
   if (user && !isError && !isLoading) {
