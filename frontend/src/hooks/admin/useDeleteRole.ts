9eef0ad8f8875b9bdea82d41395491b926f31369
Geno Racklin Asher <geno.racklinasher@gmail.com>
2022-03-28 19:30:17 +0100

    Delete role UI

---
+++ frontend/src/hooks/admin/useDeleteRole.ts
@@ -0,0 +1,24 @@
+import { useToast } from "@chakra-ui/react";
+import { useQueryClient } from "react-query";
+import { api } from "../../config/api";
+import { useCustomMutation } from "../mutations/useCustomMutation";
+
+export function useDeleteRole(id: number) {
+  const queryClient = useQueryClient();
+  const toast = useToast();
+  return useCustomMutation(
+    () => {
+      return api.delete<void>(`admin/roles/${id}`);
+    },
+    {
+      onSuccess() {
+        queryClient.invalidateQueries("admin/roles");
+        queryClient.invalidateQueries("admin/userRoles");
+        toast({
+          title: "Role deleted successfully",
+          status: "success",
+        });
+      },
+    }
+  );
+}
--- frontend/src/views/admin/AdminEditRoleView.tsx
+++ frontend/src/views/admin/AdminEditRoleView.tsx
@@ -1,31 +1,118 @@
-import { Container, Heading } from "@chakra-ui/react";
-import { useParams } from "react-router-dom";
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Icon,
+  IconButton,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { useRef } from "react";
+import { FaPen, FaTrashAlt } from "react-icons/fa";
+import { useNavigate, useParams } from "react-router-dom";
 import { PermissionsTable } from "../../components/admin/PermissionsTable";
 import { BackButton } from "../../components/utility/BackButton";
 import { Card } from "../../components/utility/Card";
+import { useDeleteRole } from "../../hooks/admin/useDeleteRole";
 import { useRoles } from "../../hooks/admin/useRoles";
+import { Role } from "../../model/Role";
 
 export function AdminEditRoleView() {
-  const {id} = useParams();
+  const { id } = useParams();
   const roleId = parseInt(id ?? "0");
   const { data, isLoading, isError } = useRoles();
   // TODO: loading states
   if (isLoading || isError || !data) {
     return <></>;
   }
-  const role = data.find(r => r.id === roleId);
+  const role = data.find((r) => r.id === roleId);
   if (!role) {
     return <></>;
   }
-  return <>
-    <Container maxW="container.md" p={0}>
-      <BackButton to="/admin/roles">Back Home</BackButton>
-      <Card mb={5}>
-        <Heading as="h3" size="lg" mb={4}>
-          {role.name}
-        </Heading>
-        <PermissionsTable role={role} />
-      </Card>
-    </Container>
-  </>
+  return (
+    <>
+      <Container maxW="container.md" p={0}>
+        <BackButton to="/admin/roles">Back Home</BackButton>
+        <Card mb={5}>
+          <Flex gap={3}>
+            <Heading as="h3" size="lg" mb={4} flex="1">
+              {role.name}
+            </Heading>
+            <IconButton
+              size="sm"
+              variant="outline"
+              // colorScheme="brand"
+              aria-label="Edit"
+              icon={<Icon as={FaPen} />}
+            ></IconButton>
+            <DeleteRoleButton role={role} />
+          </Flex>
+          <PermissionsTable role={role} />
+        </Card>
+      </Container>
+    </>
+  );
+}
+
+interface RoleProps {
+  role: Role;
+}
+
+function DeleteRoleButton({ role }: RoleProps) {
+  const mutation = useDeleteRole(role.id);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const navigate = useNavigate();
+  const cancelRef = useRef(null);
+  return (
+    <>
+      <IconButton
+        onClick={onOpen}
+        size="sm"
+        colorScheme="red"
+        aria-label="Delete"
+        icon={<Icon as={FaTrashAlt} />}
+      ></IconButton>
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Delete Role
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              Are you sure? This can't be undone.
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button
+                colorScheme="red"
+                onClick={async () => {
+                  await mutation.mutateAsync();
+                  onClose();
+                  navigate("/admin/roles");
+                }}
+                ml={3}
+                isLoading={mutation.isLoading}
+              >
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
+  );
 }
