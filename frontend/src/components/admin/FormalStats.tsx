19f7355118ab59edefae899de3204d924a467b9f
Geno Racklin Asher <geno.racklinasher@gmail.com>
2022-04-21 14:57:21 +0100

    Formal stats

--- frontend/src/components/admin/FormalStats.tsx
+++ frontend/src/components/admin/FormalStats.tsx
@@ -7,17 +7,90 @@ import {
   AlertDialogHeader,
   AlertDialogOverlay,
   AlertIcon,
+  background,
   Button,
   Heading,
   Icon,
+  Table,
+  Tbody,
+  Td,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+  useColorModeValue,
   useDisclosure,
 } from "@chakra-ui/react";
-import { useRef } from "react";
+import { useMemo, useRef } from "react";
 import { FaTrashAlt } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import { Column, TableInstance, useTable } from "react-table";
 import { useDeleteFormal } from "../../hooks/admin/useDeleteFormal";
 import { useHasPermission } from "../../hooks/admin/useHasPermission";
 import { Formal } from "../../model/Formal";
+
+interface Stat {
+  type: string;
+  count: number;
+}
+
+function typeStats(formal: Formal): Stat[] {
+  const stats = {
+    kings: 0,
+    guest: 0,
+    kingsManual: 0,
+    guestManual: 0,
+    complimentary: 0,
+    ents: 0,
+  };
+  formal.ticketSales?.forEach((t) => {
+    if (t.isGuest) {
+      stats.guest++;
+    } else {
+      stats.kings++;
+    }
+  });
+  formal.manualTickets?.forEach((t) => {
+    switch (t.type) {
+      case "standard":
+        stats.kingsManual++;
+        break;
+      case "guest":
+        stats.guestManual++;
+        break;
+      case "complimentary":
+        stats.complimentary++;
+        break;
+      case "ents":
+        stats.ents++;
+        break;
+    }
+  });
+  return [
+    { type: "King's", count: stats.kings },
+    { type: "Guests", count: stats.guest },
+    { type: "King's (Manual)", count: stats.kingsManual },
+    { type: "Guests (Manual)", count: stats.guestManual },
+    { type: "Complimentary", count: stats.complimentary },
+    { type: "Ents", count: stats.ents },
+  ];
+}
+
+function mealStats(formal: Formal): Stat[] {
+  const stats = new Map<string, number>();
+  formal.ticketSales?.forEach(t => {
+    const count = stats.get(t.option) || 0;
+    stats.set(t.option, count + 1);
+  });
+  formal.manualTickets?.forEach(t => {
+    const count = stats.get(t.option) || 0;
+    stats.set(t.option, count + 1);
+  });
+  return Array.from(stats.entries()).map(([option, count]) => ({
+    type: option,
+    count,
+  }));
+}
 
 interface FormalProps {
   formal: Formal;
@@ -25,19 +98,142 @@ interface FormalProps {
 
 export function FormalStats({ formal }: FormalProps) {
   const canDelete = useHasPermission("formals", "delete");
-  return <>
-    {canDelete && <FormalActions formal={formal}/>}
-  </>
+  const typeColumns = useMemo<Column<Stat>[]>(
+    () => [
+      {
+        Header: "Type",
+        accessor: "type",
+        Footer: "Total",
+        isNumeric: false,
+      },
+      {
+        Header: "Count",
+        accessor: "count",
+        Footer: (table) => {
+          return table.rows.reduce((acc, row) => acc + row.values.count, 0);
+        },
+        isNumeric: true,
+      },
+    ],
+    []
+  );
+  const typeData = useMemo(() => typeStats(formal), [formal]);
+  const mealColumns = useMemo<Column<Stat>[]>(
+    () => [
+      {
+        Header: "Option",
+        accessor: "type",
+        Footer: "Total",
+        isNumeric: false,
+      },
+      {
+        Header: "Count",
+        accessor: "count",
+        Footer: (table) => {
+          return table.rows.reduce((acc, row) => acc + row.values.count, 0);
+        },
+        isNumeric: true,
+      },
+    ],
+    []
+  );
+  const mealData = useMemo(() => mealStats(formal), [formal]);
+  const typeTable = useTable({
+    columns: typeColumns,
+    data: typeData,
+  });
+  const mealTable = useTable({
+    columns: mealColumns,
+    data: mealData,
+  });
+  return (
+    <>
+      <Heading as="h4" size="md" mb={1}>Ticket Types</Heading>
+      <StatTable table={typeTable} />
+      <Heading as="h4" size="md" mt={4} mb={1}>Meal Options</Heading>
+      <StatTable table={mealTable} />
+      {canDelete && <FormalActions formal={formal} />}
+    </>
+  );
 }
 
-function FormalActions({formal}: FormalProps) {
+interface StatTableProps<T extends object> {
+  table: TableInstance<T>;
+}
+
+function StatTable<T extends object>({ table }: StatTableProps<T>) {
+  const {
+    getTableBodyProps,
+    getTableProps,
+    prepareRow,
+    headerGroups,
+    footerGroups,
+    rows,
+  } = table;
+  const background = useColorModeValue("white", "gray.750");
+  return (
+    <Table my={2} variant="striped" size="sm" {...getTableProps()}>
+      <Thead>
+        {headerGroups.map((headerGroup) => (
+          <Tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <Th
+                {...column.getHeaderProps()}
+                isNumeric={(column as any).isNumeric}
+              >
+                {column.render("Header")}
+              </Th>
+            ))}
+          </Tr>
+        ))}
+      </Thead>
+      <Tbody {...getTableBodyProps()} bg={background}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <Tr {...row.getRowProps()}>
+              {row.cells.map((cell) => (
+                <Td
+                  {...cell.getCellProps()}
+                  isNumeric={(cell.column as any).isNumeric}
+                >
+                  {cell.render("Cell")}
+                </Td>
+              ))}
+            </Tr>
+          );
+        })}
+      </Tbody>
+      <Tfoot>
+        {footerGroups.map((footerGroup) => (
+          <Tr {...footerGroup.getFooterGroupProps()}>
+            {footerGroup.headers.map((column) => (
+              <Th
+                {...column.getFooterProps()}
+                pt={3}
+                isNumeric={(column as any).isNumeric}
+              >
+                {column.render("Footer")}
+              </Th>
+            ))}
+          </Tr>
+        ))}
+      </Tfoot>
+    </Table>
+  );
+}
+
+function FormalActions({ formal }: FormalProps) {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const cancelRef = useRef(null);
   const mutation = useDeleteFormal(formal.id);
   const navigate = useNavigate();
   return (
     <>
-      <Alert status="warning" mb={4} variant="left-accent">
+      <Heading as="h4" size="md" mt={4}>
+        Actions
+      </Heading>
+      <Alert status="warning" mb={4} mt={2} variant="left-accent">
         <AlertIcon />
         The following actions are potentially destructive! Only proceed if you
         know what you're doing.
